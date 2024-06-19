94aeaec3629a3c7e2b63b1ce6fd7704e1730bcdf
rexmainimo <115979693+rexmainimo@users.noreply.github.com>
2024-06-19 08:47:42 +0200

    final fixed

--- src/classes/Owner.js
+++ src/classes/Owner.js
@@ -4,27 +4,6 @@ export default class Owner{
         this.ownerSpots = [];
     }
     
-
-    // async getOwnerSpots(id) {
-    //     try{
-    //         const response = await fetch(`http://localhost:5156/CampingSpot/ownerId?id=${id}`,{
-    //             method: "GET",
-    //             headers:{
-    //                 "content-Type": "application/json",
-    //             }
-    //         });
-    //         if(response.ok){
-    //             const spots = await response.json();
-    //             this.ownerSpots = spots;
-    //             return this.ownerSpots;
-    //         }else{
-    //             console.log("Failed to retrieve owner camping spot(s)", response.statusText);
-    //         }
-    //     }
-    //     catch(error){
-    //         console.error("Error getting owner camping Spots", error);
-    //     }
-    // }
     async fetchOwnerCampingSpots(id) {
         try {
           const response = await fetch(`https://localhost:7156/CampingSpot/ownerId?id=${id}`, { 
@@ -45,22 +24,23 @@ export default class Owner{
         }
     }
     async addCampingSpot(spot) {
-        try{
-            const response = await fetch("https://localhost:7156/CampingSpot", {
-                method: "POST",
-                body: spot
-            });
-            if(response.ok){
-                const feedback = await response.json();
-                console.log(feedback.message);
-                return true;
-            }else{
-                console.log("Failed to add camping spot", await response.json());
-                return false;
-            }
-        }catch(error) {
-            console.error(error);
-            
-        }
-    }
+      try {
+          const response = await fetch("https://localhost:7156/CampingSpot", {
+              method: "POST",
+              body: spot
+          });
+          if (response.ok) {
+              const feedback = await response.json();
+              console.log(feedback)
+              return feedback; 
+          } else {
+              const error = await response.json();
+              console.error("Failed to add camping spot", error);
+              return null;
+          }
+      } catch (error) {
+          console.error("Error adding camping spot", error);
+          return null;
+      }
+  }  
 }
--- src/components/ownerSpotComponent.vue
+++ src/components/ownerSpotComponent.vue
@@ -1,189 +1,173 @@
 <template>
-    <div>
-      <button @click="toggleForm">{{ addSpot ? 'Close' : 'Add Spot' }}</button>
-      <div v-if="addSpot">
-        <message-box :message="message" />
-        <form @submit.prevent="handleSubmit" class="submit-form">
-          <h2>Add Camping Spot</h2>
-          <div class="form-group">
-            <label for="name">Name:</label>
-            <input type="text" id="name" v-model="name" required placeholder="Camping spot name">
-          </div>
-          <div class="form-group">
-            <label for="city">City:</label>
-            <input type="text" id="city" v-model="city" required>
-          </div>
-          <div class="form-group">
-            <label for="postal">Postal Number:</label>
-            <input type="text" id="postal" v-model="postal" required>
-          </div>
-          <div class="form-group">
-            <label for="street">Street Number:</label>
-            <input type="text" id="street" v-model="street" required>
-          </div>
-          <div class="form-group">
-            <label for="house">House Number:</label>
-            <input type="text" id="house" v-model="house">
-          </div>
-          <div class="form-group">
-            <label for="description">Description:</label>
-            <input type="text" id="description" v-model="description">
-          </div>
-          <div class="form-group">
-            <label for="facilities">Facilities:</label>
-            <input type="text" id="facilities" v-model="facilities">
-          </div>
-          <div class="form-group">
-            <label for="availability">Available:</label>
-            <select id="availability" v-model="availability">
-              <option value="available">Yes</option>
-              <option value="unavailable">No</option>
-            </select>
-          </div>
-          <div class="form-group">
-            <label for="image">Upload Image:</label>
-            <input type="file" id="image" @change="handleImageUpload">
-          </div>
-          <button type="submit">Submit</button>
-        </form>
-      </div>
-      <div v-else class="container_spots">
-        <div v-for="spot in ownerCampintSpots" :key="spot.id" class="camping-spot">
-          <h2>{{ spot.name }}</h2>
-          <p>Camping Spot Number: {{ spot.id }}</p>
-          <p>City: {{ spot.city }}</p>
-          <p>Postal Number: {{ spot.postalNum }}</p>
-          <p>Street Number: {{ spot.streetNum }}</p>
-          <p>House Number: {{ spot.houseNum }}</p>
-          <p>Description: {{ spot.description }}</p>
-          <p>Facilities: {{ spot.facilities }}</p>
-          <p>Availability: {{ spot.availability }}</p>
-          <p>Owner ID: {{ spot.owner_Id }}</p>
-          <img :src="`data:${spot.imageContentType};base64,${spot.imageData}`" alt="Camping Spot Image" v-if="spot.imageData"/>
+  <div>
+    <button @click="toggleForm">{{ addSpot ? 'Close' : 'Add Spot' }}</button>
+    <div v-if="addSpot">
+      <message-box :message="message" />
+      <form @submit.prevent="handleSubmit" class="submit-form">
+        <h2>Add Camping Spot</h2>
+        <div class="form-group">
+          <label for="name">Name:</label>
+          <input type="text" id="name" v-model="name" required placeholder="Camping spot name">
         </div>
+        <div class="form-group">
+          <label for="city">City:</label>
+          <input type="text" id="city" v-model="city" required>
+        </div>
+        <div class="form-group">
+          <label for="postal">Postal Number:</label>
+          <input type="text" id="postal" v-model="postalNum" required>
+        </div>
+        <div class="form-group">
+          <label for="street">Street Number:</label>
+          <input type="text" id="street" v-model="streetNum" required>
+        </div>
+        <div class="form-group">
+          <label for="house">House Number:</label>
+          <input type="text" id="house" v-model="houseNum">
+        </div>
+        <div class="form-group">
+          <label for="description">Description:</label>
+          <input type="text" id="description" v-model="description">
+        </div>
+        <div class="form-group">
+          <label for="facilities">Facilities:</label>
+          <input type="text" id="facilities" v-model="facilities">
+        </div>
+        <div class="form-group">
+          <label for="availability">Available:</label>
+          <select id="availability" v-model="availability">
+            <option value="available">Yes</option>
+            <option value="unavailable">No</option>
+          </select>
+        </div>
+        <div class="form-group">
+          <label for="image">Upload Image:</label>
+          <input type="file" id="image" @change="handleImageUpload">
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+    <div v-else class="container_spots">
+      <div v-for="spot in ownerCampintSpots" :key="spot.id" class="camping-spot">
+        <h2>{{ spot.name }}</h2>
+        <p>Camping Spot Number: {{ spot.id }}</p>
+        <p>City: {{ spot.city }}</p>
+        <p>Postal Number: {{ spot.postalNum }}</p>
+        <p>Street Number: {{ spot.streetNum }}</p>
+        <p>House Number: {{ spot.houseNum }}</p>
+        <p>Description: {{ spot.description }}</p>
+        <p>Facilities: {{ spot.facilities }}</p>
+        <p>Availability: {{ spot.availability }}</p>
+        <p>Owner ID: {{ spot.owner_Id }}</p>
+        <img :src="`data:${spot.imageContentType};base64,${spot.imageData}`" alt="Camping Spot Image" v-if="spot.imageData"/>
       </div>
     </div>
-  </template>
-  
-  <script>
-  import Owner from "../classes/Owner.js";
-  import messageBox from '../components/messageBox.vue';
+  </div>
+</template>
 
-  export default {
-    components:{
-      messageBox
-    },
-    data() {
-      return {
-        city: "",
-        postalNum: "",
-        streetNum: "",
-        houseNum: "",
-        ownerId: null,
-        addSpot: false,
-        name: "",
-        description: "",
-        facilities: "",
-        availability: "available",
-        imageFile: null,
-        submittedData: null,
-        ownerCampintSpots: [],
-        // spotImage: null,
-        message: null
-      };
-    },
-    created() {
-      this.ownerId = localStorage.getItem("ownerId");
-      if(this.ownerId){
-        this.ownerSpots();
-      }
-    },
-    methods: {
-      toggleForm() {
-        this.addSpot = !this.addSpot;
-      },
-      async ownerSpots() {
-        let owner = new Owner();
-        try {
-          this.ownerCampintSpots = await owner.fetchOwnerCampingSpots(this.ownerId);
-        } catch (error) {
-          console.error("Error accessing spots", error);
-        }
-      },
-      handleImageUpload(event) {
-        const file = event.target.files[0];
-        if (file) {
-          const reader = new FileReader();
-          reader.onload = (e) => {
-            this.imageFile = e.target.result;
-          };
-          reader.readAsDataURL(file);
-        }
-      },
-      async handleSubmit() {
-        // const newSpot = {
-        //   id: 0,
-        //   name: this.name,
-        //   city: this.city,
-        //   postalNum: this.postal,
-        //   streetNum: Number(this.street),
-        //   houseNum: this.house ? Number(this.house) : null,
-        //   description: this.description,
-        //   facilities: this.facilities,
-        //   availability: this.availability,
-        //   image: "site/pic.jpg",
-        //   owner_Id: Number(this.ownerId)
-        // };
-          const formData = new FormData();
-          formData.append('Name', this.name);
-          formData.append('City', this.city);
-          formData.append('PostalNum', this.postalNum);
-          formData.append('StreetNum', this.streetNum);
-          formData.append('HouseNum', this.houseNum);
-          formData.append('Description', this.description);
-          formData.append('Facilities', this.facilities);
-          formData.append('Availability', this.availability);
-          formData.append('ImageFile', this.imageFile);
-          formData.append('Owner_Id', this.ownerId);
-        
-        const newOwner = new Owner();
-        try {
-          const success = await newOwner.addCampingSpot(formData);
-          if (success) {
-            this.ownerCampintSpots.push(formData);
-            this.message = "spot added successully";
-            setTimeout(() => {
-              this.message = null;
-              this.resetForm();
-              this.toggleForm();
-            },2000);
-            
-          } else {
-            this.message = "Failed to add camping spot";
-            setTimeout(() => {
-              this.message = null;
-              this.resetForm();
-              this.toggleForm();
-            },2000);
-          }
-        } catch (error) {
-          console.error("Error adding camping spot", error);
-        }
-      },
-      resetForm() {
-        this.name = "";
-        this.city = "";
-        this.postal = "";
-        this.street = "";
-        this.house = "";
-        this.description = "";
-        this.facilities = "";
-        this.availability = "available";
-        this.imageFile = null;
-      }
+  
+<script>
+import Owner from "../classes/Owner.js";
+import messageBox from '../components/messageBox.vue';
+
+export default {
+  components: {
+    messageBox
+  },
+  data() {
+    return {
+      city: "",
+      postalNum: "",
+      streetNum: "",
+      houseNum: "",
+      ownerId: null,
+      addSpot: false,
+      name: "",
+      description: "",
+      facilities: "",
+      availability: "available",
+      imageFile: null,
+      submittedData: null,
+      ownerCampintSpots: [],
+      message: null
+    };
+  },
+  created() {
+    this.ownerId = localStorage.getItem("ownerId");
+    if (this.ownerId) {
+      this.ownerSpots();
     }
-  };
-  </script>
+  },
+  methods: {
+    toggleForm() {
+      this.addSpot = !this.addSpot;
+    },
+    async ownerSpots() {
+      let owner = new Owner();
+      try {
+        this.ownerCampintSpots = await owner.fetchOwnerCampingSpots(this.ownerId);
+      } catch (error) {
+        console.error("Error accessing spots", error);
+      }
+    },
+    handleImageUpload(event) {
+      const file = event.target.files[0];
+      if (file) {
+        this.imageFile = file;
+      }
+    },
+    async handleSubmit() {
+      const formData = new FormData();
+      formData.append('Name', this.name);
+      formData.append('City', this.city);
+      formData.append('PostalNum', this.postalNum);
+      formData.append('StreetNum', this.streetNum);
+      formData.append('HouseNum', this.houseNum);
+      formData.append('Description', this.description);
+      formData.append('Facilities', this.facilities);
+      formData.append('Availability', this.availability);
+      formData.append('ImageFile', this.imageFile);
+      formData.append('Owner_Id', this.ownerId);
+
+      const newOwner = new Owner();
+      try {
+        const newSpot = await newOwner.addCampingSpot(formData);
+        if (newSpot) {
+          this.ownerCampintSpots.push(newSpot); 
+          this.message = "Spot added successfully";
+          setTimeout(() => {
+            this.message = null;
+            this.resetForm();
+            this.toggleForm();
+          }, 2000);
+        } else {
+          this.message = "Failed to add camping spot";
+          setTimeout(() => {
+            this.message = null;
+            this.resetForm();
+            this.toggleForm();
+          }, 2000);
+        }
+      } catch (error) {
+        console.error("Error adding camping spot", error);
+      }
+    },
+    resetForm() {
+      this.name = "";
+      this.city = "";
+      this.postalNum = "";
+      this.streetNum = "";
+      this.houseNum = "";
+      this.description = "";
+      this.facilities = "";
+      this.availability = "available";
+      this.imageFile = null;
+    }
+  }
+};
+</script>
+
+
   
   <style scoped>
   .submit-form {
